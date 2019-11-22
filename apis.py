bcb22d84e7cd99725dd27bb268fbdc2540c61285
RationalityEnhancement <57194385+RationalityEnhancement@users.noreply.github.com>
2019-11-22 17:55:43 +0100

    fixed random function arguments

--- apis.py
+++ apis.py
@@ -11,13 +11,13 @@ def assign_constant_points(projects, default_task=10):
             child["reward"] = default_task
     return projects
 
-def assign_random_points(projects, distribution_fxn = np.random.normal, mean = 10, std = 2, *args):
+def assign_random_points(projects, distribution_fxn = np.random.normal, fxn_args = (10,2)):
     '''
     Takes in parsed project tree, with one level of tasks
     Outputs project tree with random points assigned according to distribution function with inputted args
     '''
     for goal in projects:
         for child in goal["ch"]:
-            child["reward"] = distribution_fxn(mean, std)
+            child["reward"] = distribution_fxn(*fxn_args)
     return projects
     
