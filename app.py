a59423737f0ce551baac1eade1b73784fed6347b
RationalityEnhancement <57194385+RationalityEnhancement@users.noreply.github.com>
2019-11-22 13:20:10 +0100

    change config file to match a heroku + cherrypy tutorial

--- app.py
+++ app.py
@@ -132,6 +132,8 @@ if __name__ == '__main__':
             'tools.sessions.on': True,
             'tools.response_headers.on': True,
             'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+            'server.socket_host': '0.0.0.0',
+            'server.socket_port': int(os.environ.get('PORT', '5000'))
         }
     }
     cherrypy.quickstart(Root(), '/', conf)
