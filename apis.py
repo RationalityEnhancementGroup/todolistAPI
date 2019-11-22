15d21f2800fa384ca319fc5e44d18fcd09de7fba
RationalityEnhancement <57194385+RationalityEnhancement@users.noreply.github.com>
2019-11-22 17:34:51 +0100

    add constant and random point generators

--- apis.py
+++ apis.py
@@ -1,3 +1,23 @@
 import numpy as np
 import pandas as pd
 
+def assign_constant_points(projects, default_task=10):
+    '''
+    Takes in parsed project tree, with one level of tasks
+    Outputs project tree with constant points assigned
+    '''
+    for goal in projects:
+        for child in goal["ch"]:
+            child["reward"] = default_task
+    return projects
+
+def assign_random_points(projects, distribution_fxn = np.random.normal, mean = 10, std = 2, *args):
+    '''
+    Takes in parsed project tree, with one level of tasks
+    Outputs project tree with random points assigned according to distribution function with inputted args
+    '''
+    for goal in projects:
+        for child in goal["ch"]:
+            child["reward"] = distribution_fxn(mean, std)
+    return projects
+    
