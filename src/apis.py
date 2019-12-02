0638ac74a69767292cd11cf6a1c46e9b59031bd2
RationalityEnhancement <reg.lab.management@gmail.com>
2019-12-02 14:08:24 +0100

    Added get_actions_and_rewards function (output: ordered list of tasks and thier values)

--- src/apis.py
+++ src/apis.py
@@ -5,6 +5,7 @@ from src.utils import tree_to_old_structure
 from todolistMDP.mdp_solvers import backward_induction
 from todolistMDP.to_do_list import *
 
+
 def assign_constant_points(projects, default_task=10):
     '''
     Takes in parsed project tree, with one level of tasks
@@ -14,6 +15,7 @@ def assign_constant_points(projects, default_task=10):
         for child in goal["ch"]:
             child["val"] = default_task
     return projects
+
 
 def assign_random_points(projects, distribution_fxn = np.random.normal, fxn_args = (10,2)):
     '''
@@ -25,20 +27,23 @@ def assign_random_points(projects, distribution_fxn = np.random.normal, fxn_args
             child["val"] = distribution_fxn(*fxn_args)
     return projects
     
+    
 def assign_hierarchical_points(projects):
     raise NotImplementedError
 
-def assign_old_api_points(projects, duration=8*60):
+
+def assign_old_api_points(projects, solver_fn, duration=8*60):
     '''
     input: parsed project tree and user num
-    output: task list of tasks to be done that day (c.f. shedulers.py)
+    output: list of tasks to be done that day (c.f. shedulers.py)
     '''
 
     old_structure = tree_to_old_structure(projects)
     mdp = ToDoListMDP(ToDoList(old_structure, start_time=0))
+    solver_fn(mdp)
     
-    actions_and_rewards = []
-
+    actions_and_rewards = get_actions_and_rewards(mdp)
+    
     state = (tuple(0 for task in mdp.get_tasks_list()),0) #starting state
     still_scheduling = True
 
@@ -59,10 +64,12 @@ def assign_old_api_points(projects, duration=8*60):
                 actions_and_rewards.append((possible_action, reward))
                 break
     
+    
     #TODO map action, reward to output structure , "val" is the key for rewards
     # task_list = task_list_from_projects(projects)      
 
     raise NotImplementedError
+
 
 def assign_length_points(projects):
     '''
@@ -74,3 +81,36 @@ def assign_length_points(projects):
         for child in goal["ch"]:
             child["val"] = child["est"]/float(value_per_minute)
     return projects
+
+
+def get_actions_and_rewards(mdp, verbose=False):
+    policy = mdp.get_optimal_policy()
+    values = mdp.get_value_function()
+    
+    state = sorted(list(policy.keys()))[0]
+    vec, tm = state
+    
+    value, action = values[state]
+    
+    if verbose:
+        print(state, action, value)
+    actions_and_rewards = [(action, value)]
+    
+    while policy[state] is not None:
+        # Get task
+        idx = policy[state]
+        task = mdp.index_to_task[idx]
+        
+        # Get next state
+        next_vec = list(vec)
+        next_vec[idx] = 1
+        
+        tm += task.get_time_est()
+        state = (tuple(next_vec), tm)
+        vec, tm = state
+    
+        if verbose:
+            print(state, action, value)
+        actions_and_rewards += [(action, value)]
+    
+    return actions_and_rewards
