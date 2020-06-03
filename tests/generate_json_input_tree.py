8791233578dd18d38f924727dfb5ef633f1341e2
Jugoslav Stojcheski <jugoslav.stojcheski@tuebingen.mpg.de>
2020-06-03 11:22:53 +0200

    Added n_goals and n_tasks in generate_json_input_tree.py

--- tests/generate_json_input_tree.py
+++ tests/generate_json_input_tree.py
@@ -6,12 +6,14 @@ from json_generator.nodes import *
 from json_generator.utils import *
 
 # Number of goals
-N_GOALS = [11]
 # N_GOALS = [10, 50, 100, 500, 1000, 2500, 5000, 7500, 10000]
+# N_GOALS = [11]
+N_GOALS = list(range(1, 11))
 
 # Number of tasks
-N_TASKS = [1]
+# N_TASKS = [1]
 # N_TASKS = [10, 50, 100, 250, 500, 750, 1000, 2500, 5000, 7500, 10000]
+N_TASKS = [25, 50, 75, 125, 150]
 
 # Today and typical working hours
 HOURS_TODAY = 12
@@ -20,13 +22,14 @@ HOURS_TYPICAL = 12
 # Deadlines
 # DEADLINES = [None]
 TODAY_DATE = datetime.today().date()
-DEADLINE_YEARS = [1, 2, 3, 5, 10, 15, 20]
+# DEADLINE_YEARS = [1, 2, 3, 5, 10, 15, 20]
+DEADLINE_YEARS = [1]
 
 # Type of input tree
-INPUT_TYPE = "dp"
+INPUT_TYPE = "smdp"
 
 # Path-related variables
-PATH_NAME = f"tests/data/{INPUT_TYPE}/"
+PATH_NAME = f"data/{INPUT_TYPE}/"
 
 
 def generate_file_name(n_goals, n_tasks, years):
@@ -44,10 +47,10 @@ for n_goals in N_GOALS:
             FILE_NAME = PATH_NAME + generate_file_name(n_goals, n_tasks, years)
             
             print(f"Generating {FILE_NAME}...", end=" ")
-        
+            
             # Reset node counter
             nodes.NODE_COUNTER = 0
-        
+            
             # Generate root node (the node that connects all the goal nodes
             ROOT_NODE = Node(id=0)
             node_id_dict = {
@@ -64,8 +67,9 @@ for n_goals in N_GOALS:
             # Generate goals
             new_nodes = node_id_dict[0].generate_nodes(
                 deadline_val=deadline_val, num_nodes=n_goals,
-                point_val=n_tasks, time_est_val=0)
-        
+                point_val=n_tasks, time_est_val=0
+            )
+            
             # Map generated goals
             map_nodes(node_id_dict, new_nodes)
             
@@ -82,24 +86,26 @@ for n_goals in N_GOALS:
             with open(FILE_NAME, "w") as file:
                 json_output = {
                     "currentIntentionsList": [],
-                    "projects": ROOT_NODE.generate_tree(),
+                    "projects":              ROOT_NODE.generate_tree(),
                     "timezoneOffsetMinutes": 0,
-                    "today_hours": [
+                    "today_hours":           [
                         {
                             "id": "_",
                             "nm": f"#HOURS_TODAY =={HOURS_TODAY}",
                             "lm": 0
                         }
                     ],
-                    "typical_hours": [
+                    "typical_hours":         [
                         {
                             "id": "_",
                             "nm": f"#HOURS_TYPICAL =={HOURS_TYPICAL}",
                             "lm": 0
                         }
                     ],
-                    "updated": 0,
-                    "userkey": "__test__"
+                    "updated":               0,
+                    "userkey":               "__test__",
+                    "n_goals":               n_goals,
+                    "n_tasks":               n_tasks
                 }
                 
                 file.writelines(json.dumps(json_output, indent=4))
